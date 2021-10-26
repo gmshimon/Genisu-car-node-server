c50b302005411dafe239a455370575a94148d6ef
GMShimon <simon.rosedale99@gmail.com>
2021-10-27 02:48:20 +0600

    genius car server added

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const {MongoClient} = require('mongodb');
+const app = express();
+app.use(cors());
+app.use(express.json());
+require('dotenv').config();
+const port = 5000;
+
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
+const uri = `mongodb+srv://${username}:${password}@cluster0.in8lp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("geniusCarDB");
+        const collection = database.collection("haiku");
+        // create a document to insert
+        app.post("/services", async (req, res) => {
+            const service = req.body;
+            const result = await collection.insertOne(service);
+            res.json(result);
+            console.log(service);
+        })
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('genius car');
+})
+
+app.listen(port, () => {
+    console.log('listening on port= ' + port);
+});
